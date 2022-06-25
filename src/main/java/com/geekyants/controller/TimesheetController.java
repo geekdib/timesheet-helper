c88d386272c32d6917323d0f062d23430c17961b
dibyaprakashpradhan <dibyaprakashpradhan@192.168.1.5>
2022-06-25 19:55:37 +0530

    reverse logs

--- src/main/java/com/geekyants/controller/TimesheetController.java
+++ src/main/java/com/geekyants/controller/TimesheetController.java
@@ -1,10 +1,14 @@
 package com.geekyants.controller;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.geekyants.entity.TimesheetFillRequest;
 import com.geekyants.entity.TimesheetUrl;
 import com.geekyants.repository.TimesheetFillRequestRepository;
 import com.geekyants.repository.TimesheetUrlRepository;
@@ -20,7 +24,11 @@ public class TimesheetController {
 	
 	@GetMapping("/logs")
 	public String getLogs(Model model) {
-		model.addAttribute("timesheets" , fillRequestRepository.findAll());
+		
+		List<TimesheetFillRequest> all = fillRequestRepository.findAll();
+		Collections.reverse(all);
+		
+		model.addAttribute("timesheets" , all);
 		
 		if(timesheetUrlRepository.findById(1)!=null) {
 			model.addAttribute("timesheetUrl" , timesheetUrlRepository.findById(1));
